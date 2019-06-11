10aa655ee2631cd64cc171102bfdc12fa1c52d84
Christian <me@kisu.li>
2019-06-11 11:06:47 +0300

    Guess this is a bit of an accessibility killer, remove.

--- header.php
+++ header.php
@@ -11,18 +11,7 @@ use \k1\Templates as T;
 <!doctype html>
 <html <?php language_attributes(); ?>>
   <head>
-    <!--
-    "Not all mobile browsers handle orientation changes in the same way.
-    For example, Mobile Safari often just zooms the page when changing
-    from portrait to landscape, instead of laying out the page as it
-    would if originally loaded in landscape. If web developers want their
-    scale settings to remain consistent when switching orientations on the
-    iPhone, they must add a maximum-scale value to prevent this zooming,
-    which has the sometimes-unwanted side effect of preventing users from zooming in."
-    https://developer.mozilla.org/en-US/docs/Mozilla/Mobile/Viewport_meta_tag#Viewport_width_and_screen_width
-    Users can enable zoom in browser settings if they want.
-    -->
-    <meta name="viewport" content="initial-scale=1, maximum-scale=1">
+    <meta name="viewport" content="initial-scale=1">
     <?php wp_head(); ?>
   </head>
   <?php
