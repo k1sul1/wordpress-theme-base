5986c19d94d4eb31ebc85ebec69abea3aad85b0c
Christian <me@kisu.li>
2019-06-29 00:34:39 +0300

    Add hiding support

--- lib/ACF.php
+++ lib/ACF.php
@@ -27,4 +27,21 @@ if (is_admin()) {
   add_filter('acf/load_field/key=field_5d163687ee8ce', $populator);
   add_filter('acf/load_field/key=field_5d163783ee8d1', $populator);
   add_filter('acf/load_field/key=field_5d16379aee8d2', $populator);
+
+  $breakpoints = json_decode(file_get_contents($templateDir . "/src/breakpoints.json"));
+  $breakpoints = array_keys((array) $breakpoints); // Only keys are needed
+
+  $options = [];
+  foreach ($breakpoints as $key) {
+    $options[$key] = strtoupper(str_replace("-", " ", $key));
+  }
+
+  $populator = function($field) use ($options) {
+    $field['instructions'] = \k1\app()->i18n->getText("Avoid hiding elements if possible, and provide an alternate element, visible only to that breakpoint.");
+    $field['choices'] = $options;
+
+    return $field;
+  };
+
+  add_filter('acf/load_field/key=field_5d16385be0a6b', $populator);
 }
