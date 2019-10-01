8820aa9acd483c2503394223802767e812fe833b
Christian Nikkanen <me@kisu.li>
2019-10-01 19:49:37 +0300

    Fix typo

--- functions.php
+++ functions.php
@@ -47,7 +47,7 @@ $strings = [
   'Title: Tag' => 'Tag',
   'Title: Archive' => 'Archive',
   'Breadcrumb: Home' => 'Home',
-  'Placeholder: Find from page',
+  'Placeholder: Find from page' => 'Find from page',
 ];
 
 foreach ($strings as $k => $v) {
